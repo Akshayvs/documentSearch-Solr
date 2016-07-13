9641412f4357e7f496cb0f0755952a48ed7fdf90
Akshay Sonawane <akshay.vs9543@gmail.com>
2016-07-13 14:21:43 -0400

    Initial structure : Couchbase data retreival logic  under implementation

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var lodash = require('lodash');
 var fs = require('fs');
-var app = express();
+
 var lookupApi = require('./lib/lookupApi');
+var indexSearch = require('./lib/indexSearch');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 function main() {
     var keys;
-    var searchFields;
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-
     app.get('/', function (req, res) {
         res.sendfile('./frontEnd/index.html');
     });
@@ -26,8 +28,24 @@ function main() {
         lookupApi.init(keys, res);
     });
 
-    app.listen(3000, function () {
-        console.log('1. SERVER ACTIVE: http://127.0.0.1:3000');
-    });
+
 }
 main();
+setTimeout(function () {
+
+    app.post('/values', function (req, res) {
+        var query = req.body;
+        console.log('QUERY IS: ' + JSON.stringify(query));
+        indexSearch(query, res);
+
+    });
+},5000)
+
+
+
+
+
+app.listen(3000, function () {
+    console.log('1. SERVER ACTIVE: http://127.0.0.1:3000');
+});
+
